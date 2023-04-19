ca887b3920b76845ed5c7f9a92a261f650beff60
dinhpx <dinhpx@vnpay.vn>
2023-04-19 18:03:25 +0700

    init commit

---
+++ app/src/main/java/com/dinhpx/test/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.dinhpx.test
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+
+
+
+
+
+    }
+}
