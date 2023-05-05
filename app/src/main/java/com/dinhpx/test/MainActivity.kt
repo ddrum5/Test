2603a78d4c7769b4d727d870060a4da66ce3d45c
dinhpx <dinhpx@vnpay.vn>
2023-05-05 09:40:51 +0700

    init commit

--- app/src/main/java/com/dinhpx/test/MainActivity.kt
+++ app/src/main/java/com/dinhpx/test/MainActivity.kt
@@ -2,12 +2,15 @@ package com.dinhpx.test
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.dinhpx.test.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
 
 
